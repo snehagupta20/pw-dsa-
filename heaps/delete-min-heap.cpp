db4ba26dc812e5e6f8ee61653b0efd2789e33c20
sneha gupta <gsneha20012004@gmail.com>
2024-10-16 22:14:54 +0530

    delete nodes from heap

--- heaps/delete-min-heap.cpp
+++ heaps/delete-min-heap.cpp
@@ -1,20 +1,37 @@
+// this approach wont work since we cannot actually traverse the min heap.
+
 #include <iostream>
 using namespace std;
 
 void deleteMinheap(int minheap[], int val, int &size){
-    for(int i=0;i<size-1;i++){
-        if(minheap[i] == val){
-            while(i<size-1){
-                minheap[i] = minheap[i+1];
-                i++;
-            }
-        }
-    }
+    swap(minheap[0], minheap[size-1]);
+    
+    minheap[size-1] = 0;
     size--;
+
+    int i=0;
+    // for(int i=0;i<size;i++){
+        while((2*i)+1 < size ){
+
+            if((2*i)+2 < size){
+                if(minheap[(2*i) + 1] > minheap[(2*i) + 2]){
+                    swap(minheap[i], minheap[(2*i) + 2]);
+                } else {
+                    swap(minheap[i], minheap[(2*i) + 1]);
+                }
+            } else {
+                swap(minheap[i], minheap[(2*i) + 1]);
+            }
+
+            i++;
+        }
+    // }
+
     return;
 }
-
+ 
 int main(){
+                 //    0 1 2 3 4 5 6 
     int minheap[10] = {1,2,3,4,5,6,7};
     int val = 1;
     int size = 7;
@@ -30,13 +47,56 @@ int main(){
 
 /*
 
+
+
+   2
+ 4   6
+7 5 3 
+
+     5
+  20    10
+40  50 30 60
+
+i-1 / 2
+
+remove 5
+swap[0,size-1]
+
+     60
+  20    10
+40  50 30 5
+
+remove last element
+
+     60
+  20    10
+40  50 30 
+
+check if the tree is minheap or nott
+arr : 60 20 10 40 50 30
+
+traverse from starting
+if root > 2i+1(left child)/2i+2(right child)
+    swap(root, min(left,right));
+
+     10
+  20    60
+40  50 30 
+
+60> 20 && 10
+swap 60,10
+
+similarly check aage.
+
+
+
 - remove that element 
 - shift all elements after that element by 1 space peeche
 
 7 delete
 
    1
- 2   3
+ 3   2
 4 5 6 7 
 
 delete 7 
