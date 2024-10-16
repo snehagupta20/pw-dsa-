70bb6f9d79e22c160843846f7bf0a553e670c0ec
sneha gupta <gsneha20012004@gmail.com>
2024-10-16 19:19:07 +0530

    insert min heap

--- heaps/inserting-min-heap.cpp
+++ heaps/inserting-min-heap.cpp
@@ -2,8 +2,31 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+void insert(int minHeap[], int &size, int value){
+    size++;
+    minHeap[size-1] = value;
+    int curr = size-1;
 
+    while(curr > 0 && minHeap[(curr-1)/2] > minHeap[curr]){
+        swap(minHeap[(curr-1)/2], minHeap[curr]);
+        curr = (curr-1)/2;
+    }
+
+    return;
+}
+
+int main(){
+    int minHeap[10] = {10,20,30,40,50,60,70};
+    int size = 7;
+    int value = 5;
+
+    insert(minHeap,size,value);
+
+    for(int i=0; i<size; i++){
+        cout<<minHeap[i]<<" ";
+    } cout<<endl;
+
+    return 0;
 }
 
 
